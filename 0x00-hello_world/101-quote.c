e0f42a0cf459cce9c425cbf622c7225fc74b2ad0
serverlessme <sophieejikeme@gmail.com>
2023-04-20 16:44:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C programme that prints a line to the standard error
- * Return: 1 (success)
+ * main - 101-quote.c
+ * description - Write a programme that prints exactly and that piece of art is useful
+ * - Dora Korpar, 2015-10-19, followerd by a new line to the standard error
+ * Return: 0 (success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
+	write(2,
+		       	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 	
