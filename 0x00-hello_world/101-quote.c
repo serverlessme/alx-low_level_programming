0492ff0d4769f1a996429496168b9b48d84990a4
serverlessme <sophieejikeme@gmail.com>
2023-04-20 16:30:13 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
 	return (1);
 }
 	
